121d44bca7a9405cf4264a311323d18e7312d9d7
giovannabbottino <giovannabbottino@gmail.com>
2023-01-13 21:36:00 -0300

    finaliza algoritmo
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-const medicos = [{
+const dataMedicos = [{
     nome: 'Mariana',
     horario: [1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0],
     especialidade: 'ortopedista'
@@ -22,7 +22,9 @@ const medicos = [{
     nome: 'Gustavo',
     horario: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     especialidade: 'neuro'
-}]
+}];
+
+var medicos = JSON.parse(JSON.stringify(dataMedicos)); 
 
 var lookup = {};
 var especialidade = [];
@@ -37,11 +39,11 @@ medicos.forEach((element) => {
 var cirurgia = [];
 
 function onLoad(){
+    var table  = document.getElementById("medico-table");
+    var tbody  = document.createElement('tbody'); 
+    table.appendChild(tbody);
+    
     medicos.forEach((element, index) => {
-        var table  = document.getElementById("medico-table");
-
-        var tbody  = document.createElement('tbody'); 
-        table.appendChild(tbody);
 
         var tr  = document.createElement('tr'); 
         tbody.appendChild(tr)
@@ -77,13 +79,13 @@ function onLoad(){
 
 function addCirurgia(){
     paciente = document.getElementById("fpaciente").value;
-    especialidade = document.getElementById("fespecialidade").value;
+    fespecialidade = document.getElementById("fespecialidade").value;
     prioridade = document.getElementById("fprioridade").value;
 
-    if (paciente != '' && especialidade != null && prioridade != null){
+    if (paciente != '' && fespecialidade != null && prioridade != null){
         cirurgia.push({
             paciente: paciente,
-            especialidade: especialidade,
+            especialidade: fespecialidade,
             prioridade: prioridade
         });
         var table  = document.getElementById("cirurgia-table");
@@ -100,7 +102,7 @@ function addCirurgia(){
 
         th = document.createElement("th");
         tr.appendChild(th)
-        th.innerHTML = especialidade;
+        th.innerHTML = fespecialidade;
 
         th = document.createElement("th");
         tr.appendChild(th)
@@ -112,41 +114,94 @@ function sortPorPrioridade(){
     cirurgia.sort(function(a,b){
         var x = a["prioridade"];
         var y = b["prioridade"];
-        return((x<y)? -1 : ((x>y)? 1 : 0));
+        return((x<y)? 1 : ((x>y)? -1 : 0));
     });
 }
-function sortPorHorario(){
-    medicos.sort(function(a,b){
-        var x = a["horario"];
-        var y = b["horario"];
-        for(i =0; i<12; i++){
+function sortPorEspecialidade(){
+    cirurgia.sort(function(a,b){
+        var x = a["especialidade"];
+        var y = b["especialidade"];
+        for(i =0; i<cirurgia.length; i++){
             return((x[i]>y[i])? -1 : (x[i]<y[i])? 1 : 0 );
         }
     })
 }
-function criaAgenda(){
-    sortPorPrioridade();
-    sortPorHorario();
 
-    var controle = false;
-
-    var table  = document.getElementById("horario-table");
-    cirurgia.forEach((marcacao) =>{
-        medicos.forEach((doutor) =>{
-            if(marcacao.especialidade == doutor.especialidade && controle == false){
-                doutor.horario.forEach((hora, j) =>{
-                    if(hora == 1  && controle == false){
-                        doutor.horario[j] = marcacao.paciente;
-                        controle = true;
-                    }
-                });
+function generateAgenda(especialista, paciente){
+    var hora = 0;
+    while(paciente.length > 0){
+        for(i = 0; i < especialista.length; i++){
+            if(especialista[i].horario[hora] == 1){
+                especialista[i].horario[hora] = paciente[0].paciente;
+                paciente.shift();
+            } if (paciente.length == 0){
+                break
             }
-        });
-        controle = false;
-    });
-    console.log(medicos);
-    document.getElementById("horarios-div").removeAttribute("hidden");
-    document.getElementById("medicos-div").setAttribute("hidden", "hidden");
-    document.getElementById("cirurgia-div").setAttribute("hidden", "hidden");
+        }
+        hora++;
+    }
+    return especialista;
 }
 
+var teste = false;
+var pacientes = {};
+var especialistas = {};
+var agenda = [];
+function criaAgenda(){
+    sortPorPrioridade();
+    sortPorEspecialidade();
+
+    delete pacientes;
+    pacientes = {};
+
+    for (const e of especialidade){
+        pacientes[e] = cirurgia.filter(function(val) {
+          return val['especialidade'].toLowerCase().indexOf(e.toLowerCase()) != -1;
+        });
+    }
+
+    especialistas = {};
+    for (i in especialidade) {
+        especialistas[especialidade[i]] = medicos.filter(function(val) {
+            return val['especialidade'].toLowerCase().indexOf(especialidade[i].toLowerCase()) != -1;
+        });
+    }
+    medicos = JSON.parse(JSON.stringify(dataMedicos)); 
+    agenda = [];
+    for (i in especialidade) {
+        agenda = agenda.concat(generateAgenda(especialistas[especialidade[i]], pacientes[especialidade[i]]));
+    }
+
+    if(teste == false){
+        document.getElementById("horarios-div").removeAttribute("hidden");
+        document.getElementById("medicos-div").setAttribute("hidden", "hidden");
+        document.getElementById("cirurgia-div").setAttribute("hidden", "hidden");
+        
+        var table  = document.getElementById("horario-table");
+        var tbody  = document.createElement('tbody'); 
+        table.appendChild(tbody);
+    
+        agenda.forEach((element, index) => {
+
+            var tr  = document.createElement('tr'); 
+            tbody.appendChild(tr)
+
+            var th = document.createElement("th");
+            tr.appendChild(th)
+            th.innerHTML = element.nome;
+
+            element.horario.forEach((item, index) => {
+                th = document.createElement("th");
+                tr.appendChild(th)
+                if (item == 1){
+                    th.innerHTML = 'Livre';
+                }else if (item == 0) {
+                    th.innerHTML = ' - ';
+                } else{
+                    th.innerHTML = item;
+                }
+            });
+            
+        });
+    }
+}
